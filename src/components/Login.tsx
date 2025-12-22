562c9bb5a18792d0d723bb835b8173bbfc5e013d
eDarkz <113264265+eDarkz@users.noreply.github.com>
2025-12-22 14:38:08 -0700

    Updated Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -36,7 +36,7 @@ export default function Login({ onLoginSuccess }: LoginProps) {
         </div>
 
         <h1 className="text-3xl font-bold text-center text-gray-800 mb-2">
-          Sistema SEPLC
+         CAS:M - INGENIERIA SEPLC
         </h1>
         <p className="text-center text-gray-600 mb-8">
           Ingresa la contraseña para acceder
